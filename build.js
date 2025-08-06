9252927722debfc720f5dd5d165303618b1051a7
Johnny Black <john.p.black89@gmail.com>
2025-08-06 14:41:44 -0400

    Updated build file to accommodate src folder move

--- build.js
+++ build.js
@@ -2,13 +2,13 @@ import { build } from "@dtrlanz/gas-bundler";
 
 await build({
 	entryPoints: [
-		"sheetContext.js",
-		"tasksContext.js",
-		"utilities.js",
-		"namedRangeHyperlinks.js",
-		"longTerm.js",
-		"holidayPrep.js",
-		"main.js",
+		"./src/sheetContext.js",
+		"./src/tasksContext.js",
+		"./src/utilities.js",
+		"./src/namedRangeHyperlinks.js",
+		"./src/longTerm.js",
+		"./src/holidayPrep.js",
+		"./src/main.js",
 	], // Order matters here
 	outdir: "dist",
 });
